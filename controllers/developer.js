ecaad07453bd4036f99e8acc4cf468e4eba214c4
Denise Case <denisecase@gmail.com>
2019-11-04 00:20:08 -0600

    initial commit

---
+++ controllers/developer.js
@@ -0,0 +1,87 @@
+/**
+*  Developer controller
+*  Handles requests related to developer resources.
+*
+* @author Denise Case <dcase@nwmissouri.edu>
+*
+*/
+const express = require('express')
+const api = express.Router()
+const Model = require('../models/developer.js')
+const find = require('lodash.find')
+const notfoundstring = 'Could not find developer with id='
+
+// RESPOND WITH JSON DATA  --------------------------------------------
+
+// GET all JSON
+api.get('/findall', (req, res) => {
+  res.setHeader('Content-Type', 'application/json')
+  const data = req.app.locals.developers.query
+  res.send(JSON.stringify(data))
+})
+
+// GET one JSON by ID
+api.get('/findone/:id', (req, res) => {
+  res.setHeader('Content-Type', 'application/json')
+  const id = parseInt(req.params.id)
+  const data = req.app.locals.developers.query
+  const item = find(data, { _id: id })
+  if (!item) { return res.end(notfoundstring + id) }
+  res.send(JSON.stringify(item))
+})
+
+// RESPOND WITH VIEWS  --------------------------------------------
+
+// GET to this controller base URI (the default)
+api.get('/', (req, res) => {
+  res.render('developer/index.ejs', {
+    developers: req.app.locals.developers.query
+  })
+})
+
+// GET create
+api.get('/create', (req, res) => {
+  res.render('developer/create', {
+    developers: req.app.locals.developers.query,
+    developer: new Model()
+  })
+})
+
+// GET /delete/:id
+api.get('/delete/:id', (req, res) => {
+  const id = parseInt(req.params.id)
+  const data = req.app.locals.developers.query
+  const item = find(data, { _id: id })
+  if (!item) { return res.end(notfoundstring + id) }
+  res.render('developer/delete', {
+    developer: item
+  })
+})
+
+// GET /details/:id
+api.get('/details/:id', (req, res) => {
+  const id = parseInt(req.params.id)
+  const data = req.app.locals.developers.query
+  const item = find(data, { _id: id })
+  if (!item) { return res.end(notfoundstring + id) }
+  res.render('developer/details', {
+    developer: item
+  })
+})
+
+// GET one
+api.get('/edit/:id', (req, res) => {
+  const id = parseInt(req.params.id)
+  const data = req.app.locals.developers.query
+  const item = find(data, { _id: id })
+  if (!item) { return res.end(notfoundstring + id) }
+  res.render('developer/edit', {
+    developer: item
+  })
+})
+
+// HANDLE EXECUTE DATA MODIFICATION REQUESTS --------------------------------------------
+
+// later
+
+module.exports = api
